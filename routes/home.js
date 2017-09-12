0b6a9937e46a522ac8caf537a7c8e2c784122ff6
DESKTOP-SD5KS9S\SANDWOOD <sandwood09@gmail.com>
2017-09-12 15:42:49 +0900

    0912 commit

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(request, response) {
-  return response.render('index.html')
+  return response.render('index.html');
 });
 
 module.exports = router;
