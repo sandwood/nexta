8397b80040746ab9fb9865d163cb8b1ed83f9d63
DESKTOP-SD5KS9S\SANDWOOD <sandwood09@gmail.com>
2017-09-15 15:48:56 +0900

    0915-4 commit

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ app.use('/robots.txt', function(req, res) {
     res.send("User-agent: *\nAllow: /");
 });
 app.listen(3000, function(){
-    console.log('Server is running at 3000!');
+    console.log('Server is running! (☉_☉)');
 })
